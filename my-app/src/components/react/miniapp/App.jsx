36a46b59322281340e82ab3eff4c0dd45c0c8439
Ljf227 <ljfrotton@gmail.com>
2023-11-29 15:17:37 -0500

    going to wrap a react-router-dom app inside to show

---
+++ my-app/src/components/react/miniapp/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+
+export default function App(){
+
+    return(
+        <main class="bg-blue-700">
+            <h1>React Mini App</h1>
+        </main>
+        )
+}
