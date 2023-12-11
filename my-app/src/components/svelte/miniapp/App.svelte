73c19fe67c01c69799c39f08c4d6919215241b8f
Ljf227 <ljfrotton@gmail.com>
2023-12-10 22:52:12 -0500

    testing more

--- my-app/src/components/svelte/miniapp/App.svelte
+++ my-app/src/components/svelte/miniapp/App.svelte
@@ -20,10 +20,12 @@
     }
 </style>
 
-<main class="min-h-screen bg-orange-500" class:advanced={state === 'advanced'}>
-    <h1>Here is a {state} Svelte app</h1>
-    <button on:click={toggleState}>{buttonText}</button>
-    {#if state === 'advanced'}
-        <p>Welcome to the advanced state!</p>
-    {/if}
+<main class="w-1/2 h-full bg-orange-500" class:advanced={state === 'advanced'}>
+    <h1 class="text-center text-3xl">Here is a {state} Svelte app</h1>
+    <div class="flex items-center flex-col justify-center">
+        <button class="text-center text-2xl p-3 m-5 text-black bg-gray-300 hover:bg-gray-500 font-extrabold rounded-md" on:click={toggleState}>{buttonText}</button>
+        {#if state === 'advanced'}
+            <p class="text-center font-bold text-xl">Welcome to the advanced state!</p>
+        {/if}
+    </div>
 </main>
